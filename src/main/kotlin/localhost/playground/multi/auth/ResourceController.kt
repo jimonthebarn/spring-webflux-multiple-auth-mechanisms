5268288605381fa646ffce9cb976440c104d4987
Benjamin Roth <broth@telekom.de>
2018-12-05 12:45:03 +0100

    Added authentication information to log statements in resource controller for easier verification.

--- src/main/kotlin/localhost/playground/multi/auth/ResourceController.kt
+++ src/main/kotlin/localhost/playground/multi/auth/ResourceController.kt
@@ -1,19 +1,39 @@
 package localhost.playground.multi.auth
 
+import mu.KotlinLogging
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+
+private val logger = KotlinLogging.logger {}
 
 @RestController
 class ResourceController {
 
     @GetMapping("/resourceA/")
-    fun resourceA(): Mono<String> {
-        return Mono.just("Accessed resource A")
+    fun resourceA(exchange: ServerWebExchange): Mono<String> {
+
+        return exchange.getPrincipal<UsernamePasswordAuthenticationToken>().map {
+
+            logger.info { "Accessed resource A. Who: ${it.principal}  Authorities: ${it.authorities} Credentials: ${it.credentials}" }
+
+            "Accessed resource A"
+        }
     }
 
     @GetMapping("/resourceB/")
-    fun resourceB(): Mono<String> {
-        return Mono.just("Accessed resource B")
+    fun resourceB(exchange: ServerWebExchange): Mono<String> {
+
+        return exchange.getPrincipal<UsernamePasswordAuthenticationToken>().map {
+
+            logger.info { "Accessed resource B. Who: ${it.principal}  Authorities: ${it.authorities} Credentials: ${it.credentials}" }
+
+            "Accessed resource B"
+        }
+
+
     }
 }
